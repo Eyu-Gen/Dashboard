8d95c3d34e407c526d7e6c91e8460b128ca21113
Eyu-Gen <dreamersthebig@gmail.com>
2024-12-29 21:36:53 +0545

    Next JS

---
+++ app/lib/definations.js
@@ -0,0 +1,15 @@
+const Invoice = {
+    id: "string",        
+    customer_id: "string", 
+    amount: 0,         
+    date: "string",     
+    status: "pending", 
+};
+
+const exampleInvoice = {
+    id: "1",
+    customer_id: "12345",
+    amount: 1000,
+    date: "2023-12-28",
+    status: "pending",
+};
